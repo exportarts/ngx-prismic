164098656871aca566840cba27df9b055be9f7cb
Enzo Volkmann <enzo@exportarts.io>
2019-03-20 14:17:41 +0100

    Added `queryOne()` method to get a single document by UID
    Additionally updated docs

--- lib/prismic-client/src/services/prismic.service.ts
+++ lib/prismic-client/src/services/prismic.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { Predicates } from 'prismic-javascript';
 import ResolvedApi, { QueryOptions } from 'prismic-javascript/d.ts/ResolvedApi';
 import { iif, Observable, of } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
-import { TypedApiSearchResponse } from './api.model';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { TypedApiSearchResponse, TypedDocument } from './api.model';
 import { PrismicServiceConfig, PrismicServiceConfigProvider } from './prismic-service.config';
 
 /**
@@ -45,6 +46,15 @@ export class PrismicService {
     }).join('');
   }
 
+  /**
+   * Query the API with the specified list of predicates and query options.
+   * Reference about the different options can be found in Prismic's docs:
+   * - https://prismic.io/docs/javascript/query-the-api/query-predicates-reference
+   * - https://prismic.io/docs/javascript/query-the-api/query-options-reference
+   * 
+   * @param predicates An array of predicate strings (Use the `Prismic.Predicates.*` methods)
+   * @param options additional options
+   */
   query<T>(predicates: string[], options: QueryOptions = {}): Observable<TypedApiSearchResponse<T>> {
     return this.api.pipe(
       switchMap(api => {
@@ -54,6 +64,23 @@ export class PrismicService {
         const url = `${this.config.prismicUrl}/documents/search?ref=${ref}${encodedQuery}${encodedOptions}`;
         return this.http.get<TypedApiSearchResponse<T>>(url);
       })
+    );
+  }
+
+  /**
+   * Query the API to retrieve a single document by it's type and UID.
+   *
+   * @param docType API-Name of the document-type
+   * @param uid The document's UID
+   * @param options additional options
+   */
+  queryOne<T>(docType: string, uid: string, options: QueryOptions = {}): Observable<TypedDocument<T>> {
+    const predicates = [
+      Predicates.at(`my.${docType}.uid`, uid)
+    ];
+
+    return this.query<T>(predicates, options).pipe(
+      map(reponse => reponse.results[0])
     );
   }
 
