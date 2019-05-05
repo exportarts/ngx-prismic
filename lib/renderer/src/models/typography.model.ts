2b5957674f0962a0ab3387959823373654edac76
Enzo Volkmann <enzo@exportarts.io>
2019-05-05 21:57:31 +0200

    Added array-variants of the typography types
    Marked existing interfaces (Paragraph, Heading1, ...) as deprecated.
    
    Prismic always returns an array of thos objects when using the "Title" or "Richtext" components in custom types. Until now, the user of this API had to manually create an array of the types provided here, which is not íntuitive.

--- lib/renderer/src/models/typography.model.ts
+++ lib/renderer/src/models/typography.model.ts
@@ -6,8 +6,7 @@ interface TextNode {
 }
 
 /**
- * The Prismic API returns an array of paragraphs when using
- * e.g. the 'Title'- or 'Rich Text'-elements.
+ * @deprecated will be internalized in next major, use Paragraphs instead
  */
 export interface Paragraph extends TextNode {
   type:
@@ -23,29 +22,56 @@ export interface Paragraph extends TextNode {
     | 'list-item';
 }
 
+export type Paragraphs = Paragraph[];
+
+/**
+ * @deprecated will be internalized in next major, use Headings1 instead
+ */
 export interface Heading1 extends TextNode {
   type: 'heading1';
 }
 
+/**
+ * @deprecated will be internalized in next major, use Headings2 instead
+ */
 export interface Heading2 extends TextNode {
   type: 'heading2';
 }
 
+/**
+ * @deprecated will be internalized in next major, use Headings3 instead
+ */
 export interface Heading3 extends TextNode {
   type: 'heading3';
 }
 
+/**
+ * @deprecated will be internalized in next major, use Headings4 instead
+ */
 export interface Heading4 extends TextNode {
   type: 'heading4';
 }
 
+/**
+ * @deprecated will be internalized in next major, use Headings5 instead
+ */
 export interface Heading5 extends TextNode {
   type: 'heading5';
 }
 
+/**
+ * @deprecated will be internalized in next major, use Headings6 instead
+ */
 export interface Heading6 extends TextNode {
   type: 'heading6';
 }
+
+export type Headings1 = Heading1[];
+export type Headings2 = Heading2[];
+export type Headings3 = Heading3[];
+export type Headings4 = Heading4[];
+export type Headings5 = Heading5[];
+export type Headings6 = Heading6[];
 
 export type Select<T extends string> = {
   type: 'Select',
