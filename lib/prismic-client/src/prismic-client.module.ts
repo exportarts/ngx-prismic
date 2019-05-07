d13b5c57727f0a267a51449e33a5f507d5a6d60a
Enzo Volkmann <enzo@exportarts.io>
2019-05-07 07:27:23 +0200

    Added renderHtmlPipe

--- lib/prismic-client/src/prismic-client.module.ts
+++ lib/prismic-client/src/prismic-client.module.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RenderHtmlPipe } from './pipes/render-html.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RenderHtmlPipe
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    RenderHtmlPipe
   ]
 })
 export class PrismicClientModule { }
