01314695e981c40f0b2ef9900d42692fe8a5f97d
Enzo Volkmann <enzo@exportarts.io>
2019-05-08 18:31:39 +0200

    Added tests for renderImage pipe

---
+++ lib/prismic-client/src/pipes/render-image.pipe.spec.ts
@@ -0,0 +1,86 @@
+import { SpanType } from '../models/span.model';
+import { Paragraphs } from '../models/typography.model';
+import { RenderImagePipe } from './render-image.pipe';
+import { Image } from '../models/image.model';
+
+describe('RenderImagePipe', () => {
+    let pipe: RenderImagePipe;
+    beforeEach(() => {
+      pipe = new RenderImagePipe();
+    });
+
+    describe('tranform()', () => {
+        test('Simple image', () => {
+            const image: Image = {
+                url: 'photo.jpg',
+                dimensions: {
+                    height: 100,
+                    width: 100
+                },
+                alt: null,
+                copyright: null
+            };
+
+            const rendered = pipe.transform(image);
+            expect(rendered).toEqual('<img src="photo.jpg">')
+        });
+
+        test('Image with title', () => {
+            const image: Image = {
+                url: 'photo.jpg',
+                dimensions: {
+                    height: 100,
+                    width: 100
+                },
+                alt: null,
+                copyright: null
+            };
+            const title = 'my photo';
+
+            const rendered = pipe.transform(image, title);
+            expect(rendered).toEqual('<img title="my photo" src="photo.jpg">')
+        });
+
+        test('Image with title and classes', () => {
+            const image: Image = {
+                url: 'photo.jpg',
+                dimensions: {
+                    height: 100,
+                    width: 100
+                },
+                alt: null,
+                copyright: null
+            };
+            const title = 'my photo';
+            const classes = 'my-image'
+
+            const rendered = pipe.transform(image, title, classes);
+            expect(rendered).toEqual('<img class="my-image" title="my photo" src="photo.jpg">')
+        });
+
+        test('Image with title, classes and caption', () => {
+            const image: Image = {
+                url: 'photo.jpg',
+                dimensions: {
+                    height: 100,
+                    width: 100
+                },
+                alt: null,
+                copyright: null
+            };
+            const title = 'my photo';
+            const classes = 'my-image'
+            const caption: Paragraphs = [
+                {
+                    text: 'Check out my pic',
+                    spans: [],
+                    type: 'paragraph'
+                }
+            ];
+
+            const rendered = pipe.transform(image, title, classes, caption);
+            expect(rendered).toEqual('<figure><img class="my-image" title="my photo" src="photo.jpg"><figcaption><p>Check out my pic</p></figcaption></figure>')
+        });
+    });
+    
+});
