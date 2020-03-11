eaa2a9b11d119f7f90cad6e5d879fff7659f1f1e
Enzo Volkmann <enzo@exportarts.io>
2020-03-11 21:31:24 +0100

    feat: add missing models for prismic api v2

--- lib/prismic-client/src/models/link.model.ts
+++ lib/prismic-client/src/models/link.model.ts
@@ -1,10 +1,33 @@
 /**
- * Represents a link to a media item.
+ * Represents a link to a media item or document.
  */
-export interface Link {
-    kind: string;
+export interface BaseLink {
     link_type: string;
+}
+
+/**
+ * @deprecated use `MediaLink` instead
+ */
+export interface Link extends BaseLink {
+    kind: string;
     name: string;
     size: string;
     url: string;
+    height?: number;
+    width?: number;
 }
+export type MediaLink = Link;
+
+export interface ContentRelationship extends Link {
+    id: string;
+    type: string;
+    tags: string[];
+    slug: string;
+    lang: string;
+    uid: string;
+    isBroken: boolean;
+}
+/**
+ * @deprecated use `ContentRelationship` instead
+ */
+export type LinkedDocument = ContentRelationship;
